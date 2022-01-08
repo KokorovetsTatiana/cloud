2e71aa9ef8691acf5d2aa548492821cde961df9d
KokorovetsTatiana <tanuska@ukr.net>
2022-01-08 21:59:16 +0300

    homepage done

--- src/components/shared/Hero/Hero.jsx
+++ src/components/shared/Hero/Hero.jsx
@@ -11,12 +11,14 @@ export default class Hero extends Component {
   render() {
     return (
     <section className={styles.hero}> 
-        <aside></aside>
-        <MainHeader text={'Personal and Business'} />
-        <SecHeadingUnderLine text={'Online storage'} />
-        <SecondaryHeading text={'STORE AND MANAGE ALL YOUR Files!'} />
-        <ParagHero text={'Upload multiple files at once and keep them forever on this site. If you are using FireFox or Chrome, you can simply drag & drop your files to begin uploading'} />    
-        <Button text={'Try SkyBox Now'} btnAction={"You are ready to try our app!"} />
+        <aside className={styles.heroPict}></aside>
+        <aside className={styles.heroInfo}>
+          <MainHeader text={'Personal and Business'} />
+          <SecHeadingUnderLine text={'Online storage'} />
+          <SecondaryHeading text={'STORE AND MANAGE ALL YOUR Files!'} />
+          <ParagHero text={'Upload multiple files at once and keep them forever on this site. If you are using FireFox or Chrome, you can simply drag & drop your files to begin uploading'} />    
+          <Button text={'Try SkyBox Now'} btnAction={"You are ready to try our app!"} />
+        </aside>
     </section>
   );
 }
